ae5dc217d493cb4cc51f8ecc3eff2e00345fbbf7
Said Boudad <74699842+SaidBoudad@users.noreply.github.com>
2023-01-13 03:44:30 +0100

    Update Student.java

--- src/main/java/com/saidboudad/demo/entity/Student.java
+++ src/main/java/com/saidboudad/demo/entity/Student.java
@@ -8,13 +8,17 @@ import javax.persistence.*;
 @Data
 @Table(name = "students")
 public class Student {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     @Column(name = "first_name",nullable = false)
     private String firstName;
+    
     @Column(name = "last_name",nullable = false)
     private String lastName;
+    
     @Column(name = "phone",nullable = false)
     private String phoneNumber;
 }
