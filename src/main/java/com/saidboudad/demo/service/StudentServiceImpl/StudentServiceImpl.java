18696b905d8d936727d8b7d9310e06d039e3d7a9
Said Boudad <74699842+SaidBoudad@users.noreply.github.com>
2023-01-13 03:45:22 +0100

    Update StudentServiceImpl.java

--- src/main/java/com/saidboudad/demo/service/StudentServiceImpl/StudentServiceImpl.java
+++ src/main/java/com/saidboudad/demo/service/StudentServiceImpl/StudentServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
 public class StudentServiceImpl implements StudentService {
+    
     //injection dependency to StudentRepository
     private StudentRepository studentRepository;
 
